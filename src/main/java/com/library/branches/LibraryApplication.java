36f1877a5a0d44bfdfd15f97cd5e0e88a89d07e2
Quty <quty.jain@gmail.com>
2018-04-18 00:18:21 -0400

    Adding service to Eureka registry

--- src/main/java/com/library/branches/LibraryApplication.java
+++ src/main/java/com/library/branches/LibraryApplication.java
@@ -3,9 +3,11 @@ package com.library.branches;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 public class LibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
