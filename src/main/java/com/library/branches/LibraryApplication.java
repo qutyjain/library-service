0c557a427a57d97d6bd455b31f7c2c86ea15456e
Quty <quty.jain@gmail.com>
2018-05-01 20:27:23 -0400

    Configuring sleuth for tracing logs d enabling zipkin

--- src/main/java/com/library/branches/LibraryApplication.java
+++ src/main/java/com/library/branches/LibraryApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RefreshScope
 @EnableEurekaClient
 @EnableCircuitBreaker
 public class LibraryApplication {
+	
+	@Bean
+	public Sampler defaultSampler(){
+		return new AlwaysSampler();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
